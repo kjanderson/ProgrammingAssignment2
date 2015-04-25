32b616f309ded31420000aea3166c29d79bb7018
kjanderson <korwin@ieee.org>
2015-04-25 14:42:21 -0700

    Change cachematrix.R to cache matrix computations as per R Programming Assignment 2.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
 ## Put comments here that give an overall description of what your
 ## functions do
+## makeCacheMatrix: create a matrix
+## cacheSolve: compute the matrix inverse
 
-## Write a short comment describing this function
+## makeCacheMatrix: create the vector to store matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+        set <- function(y)
+        {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(fcnInverse) m <<- fcnInverse
+        getinverse <- function() m
+        list(set = set,
+             get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve: calculate matrix inverse if cache misses
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        m <- x$getinverse()
+        if(!is.null(m))
+        {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setinverse(m)
+        m
 }
+
